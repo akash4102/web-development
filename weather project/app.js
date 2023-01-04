b496685d21c1fd373643d1fd1d24ddf04327c77d
AKASH VERMA <aakashverma4102@gmail.com>
2023-01-04 23:00:36 +0530

    add something

--- weather project/app.js
+++ weather project/app.js
@@ -1,16 +1,38 @@
 const express=require("express");
 const https=require("https");
+const bodyParser=require("body-parser");
 
 const app=express();
 
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
-    const url="https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=b7a5d4e3ae8d5754060cb45ab173c2eb";
-    https.get(url,function(response){
-        console.log(response);
-    });
-    res.send("server is running");
+    res.sendFile(__dirname+"/index.html");
 });
+
+app.post("/",function(req,res){
+    const city=req.body.cityName;
+    const query=city;
+    const apiKey="b7a5d4e3ae8d5754060cb45ab173c2eb";
+    const unit="metric";
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
+    https.get(url,function(response){
+        response.on("data",function(data){
+            const weatherData=JSON.parse(data);
+            const temp=weatherData.main.temp
+            const weatherDescription=weatherData.weather[0].description;
+            const icon=weatherData.weather[0].icon;
+            const imgUrl="https://openweathermap.org/img/wn/"+icon+"@2x.png";
+            res.write("<p>the Weather is currently"+weatherDescription +"</p>");
+            res.write("<h1>the temperature in " + query +" is "+temp+"desgrees celcius.</h1>")
+            res.write('<img src="' + imgUrl + '">');
+            res.send();
+        });
+    });
+});
+
+
+
 
 
 
