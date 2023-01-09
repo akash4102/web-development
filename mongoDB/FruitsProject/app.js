37b47bb1067bafd3c7de7fe7ca23f2b57d404660
AKASH VERMA <aakashverma4102@gmail.com>
2023-01-09 21:47:14 +0530

    add database to to do list

--- mongoDB/FruitsProject/app.js
+++ mongoDB/FruitsProject/app.js
@@ -1,46 +1,131 @@
 // jshint eseversion:6
 
-const MongoClient=require("mongodb").MongoClient;
-const assert=require("assert");
+const mongoose=require('mongoose');
 
-const url='mongodb://localhost:27017';
+const url='mongodb://localhost:27017/fruitsDB';
+mongoose.set('strictQuery',false);
+mongoose.connect(url, { useNewUrlParser: true });
 
-const dbName='fruitsDB';
-const client=new MongoClient(url);
-
-client.connect(function(err){
-    assert.equal(null,err);
-    console.log("connected successfully to server");
-
-    const db=client.db(dbName);
-    insertDocuments(db,function(){
-        client.close();
-    });
+const fruitSchema=new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "please check your data entry no name specified"]
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
+    review: String
 });
 
-const insertDocuments=function(db,callback){
-    const collection=db.collection('fruits');
-    collection.insertMany([
-        {
-            name:"apple",
-            score:8,
-            review:"gread fruit"
-        },
-        {
-            name:"orange",
-            score:6,
-            review:"kinda sour"
-        },
-        {
-            name:"banana",
-            score:9,
-            review:"great Stuff"
-        }
-    ],function(err,result){
-        assert.equal(err,null);
-        assert.equal(3,result.result.n);
-        assert.equal(3,result.ops.length);
-        console.log("inserted 3 doucments into teh collection");
-        callback(result);
-    });
-};
+
+const Fruit =mongoose.model("Fruit",fruitSchema);
+
+// const fruit = new Fruit ({
+//     name: "mango",
+//     rating: 10,
+//     review: "peaches are so lovely."
+// });
+
+// fruit.save();
+
+const peopleSchema=new mongoose.Schema({
+    name: String,
+    age: Number,
+    favouriteFruit: fruitSchema
+});
+
+const guava=new Fruit({
+    name:"guava",
+    rating: 8,
+    review: "great fruit for winters"
+})
+
+guava.save();
+const People=mongoose.model("People",peopleSchema);
+
+// const people=new People({
+//     name: "sachin",
+//     age: 12,
+//     favouriteFruit: pineapple
+// });
+
+// people.save();
+
+// const kiwi=new Fruit({
+//     name: "kiwi",
+//     score: 10,
+//     review: "the best fruit!"
+// });
+// const orange=new Fruit({
+//     name: "orange",
+//     score: 4,
+//     review: "too sour for me"
+// });
+// const banana=new Fruit({
+//     name: "banana",
+//     score: 3,
+//     review: "weird texture"
+// });
+
+// Fruit.insertMany([kiwi,orange,banana],function(err){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("succesfully saved all the fruits to fruitsDB");
+//     }
+// });
+
+// Fruit.find(function(err,fruits){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         // console.log(fruits);
+//         mongoose.connection.close();
+//         let arrayOfFruits=fruits;
+//         arrayOfFruits.forEach(element => {
+//             console.log(element.name);
+//         });
+//     }
+// });
+
+// Fruit.updateOne({name:"mango"} , {rating:42},function(err){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("updation success");
+//     }
+// });
+
+// Fruit.deleteOne({name:"peach"},function(err){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("successfully deleted");
+//     }
+// });
+
+// People.deleteMany({name:'Akash'},(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("deletion successful");
+//     }
+// });
+
+People.updateOne({name:"Akash"} , {favouriteFruit:guava},function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("updation success");
+    }
+});
+
+
--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,34 +3,117 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const { application } = require("express");
-const date=require(__dirname+"/date.js");
+const mongoose = require("mongoose");
+const { closeDelimiter } = require("ejs");
+const _ =require("lodash");
+
 const app=express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+mongoose.set('strictQuery',false);
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true});
 
+const itemsSchema ={
+    name: {
+        type: String,
+        required: [true, "please add atleast one work"]
+    }
+}
 
-let items=["buy","sleep"];
-let workItems=[];
-const days=["sunday","monday","tuesday","wednesday","thrusday","friday","saturday","sunday"];
+const Item=mongoose.model('Item',itemsSchema);
+
+const item1= new Item({
+    name: "Welcome to your todolist"
+});
+const item2= new Item({
+    name: "Hit the + button to add a new item."
+});
+const item3= new Item({
+    name: "<-- Hit this to delete an item"
+});
+const defaultItems=[item1,item2,item3];
+
+const listSchema={
+    name: String,
+    items: [itemsSchema]
+};
+const List=mongoose.model("List",listSchema);
+
 app.get("/",function(req,res){
-    res.render('list',{listTitle:date.getDay(),newListItems:items});
+    Item.find((err,items)=>{
+        if(items.length===0){
+            Item.insertMany(defaultItems,(err)=>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log("successfully saved default items to DB");
+                }
+            });
+            res.redirect('/');
+        }
+        else{
+            res.render('list',{listTitle:"Today",newListItems:items});
+        }
+    });
 });
 app.post("/",(req,res)=>{
-    item=req.body.work;
-    if(req.body.list==="work"){
-        workItems.push(item);
-        res.redirect("/work");
+    const itemName=req.body.work;
+    const listName=req.body.list
+    const item=new Item({
+        name: itemName
+    });
+    if(listName==="Today"){
+        item.save();
+        res.redirect('/');
     }
     else{
-        items.push(item);
-        res.redirect("/");
+        List.findOne({name:listName},function(err,foundList){
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/"+listName);
+        });
     }
-    res.redirect("/");
 });
 
-app.get("/work",(req,res)=>{
-    res.render('list',{listTitle:"work List",newListItems:workItems});
+app.post("/delete",(req,res)=>{
+    const checkedItemId=req.body.checkbox;
+    const listName=req.body.listName;
+    if(listName==="Today"){
+        Item.findByIdAndRemove(checkedItemId,function(err){
+            if(!err){
+                console.log("successfully deleted")
+                res.redirect("/");
+            }
+        });
+    }
+    else{
+        List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkedItemId}}},function(err,foundList){
+            if(!err){
+                res.redirect("/"+listName);
+            }
+        });   
+    }
+});
+
+app.get('/:customListName',(req,res)=>{
+    const customListName=_.capitalize(req.params.customListName);
+    List.findOne({name:customListName},function(err,foundList){
+        if(!err){
+            if(!foundList){
+                const list=new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+                list.save();
+                res.redirect("/"+customListName);
+            }
+            else {
+                res.render("list",{listTitle:customListName,newListItems:foundList.items})
+            }
+        }
+    })
 });
 
 app.get("/about",(req,res)=>{
